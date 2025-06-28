9d5a65787ebfc56e6cb2bea3e583c4e43e9e3b41
Rohit Kamble <rohitkamblein2020@gm,ail.com>
2025-06-28 23:15:10 +0530

    Initial Setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <div>
+    <h1>Welcome to My React App</h1>
+  </div>
+  </React.StrictMode>
+);
